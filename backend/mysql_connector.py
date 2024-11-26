d93398481646bf6b1915e14f1275a79f14537d00
Affaan Jaweed <affaan190@gmail.com>
2024-11-26 15:12:11 +0530

    added add product and delete product functionality to product_doa and changed connection object settings

--- backend/mysql_connector.py
+++ backend/mysql_connector.py
@@ -1,6 +1,13 @@
+import datetime
 import mysql.connector
-__con_obj=None
+
+__cnx = None
+
 def sql_con_obj():
-    __con_obj=mysql.connector.connect(user='root', password='1234',host='127.0.0.1',database='store')
-    if __con_obj==None:
-        return __con_obj
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='1234',host = '127.0.0.1', database='store')
+
+  return __cnx
--- backend/python_doa.py
+++ backend/python_doa.py
@@ -1,4 +1,5 @@
 from mysql_connector import sql_con_obj
+import mysql_connector
 def get_all(connection):
     
     cursor=connection.cursor()
@@ -18,7 +19,29 @@ def get_all(connection):
         )
     return response
     con_obj.close()
+def add_product(connection,product):
+    cursor=connection.cursor()
+    query='INSERT INTO products (product_name, um_id, price_per_unit) VALUES (%s,%s,%s);'
+    data=(product['product_name'],product['um_id'],product['price_per_unit'])
+    cursor.execute(query,data)
+    connection.commit()
+    return cursor.lastrowid
+
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+    return cursor.lastrowid
+
 
 if __name__=='__main__':
     connection=sql_con_obj()
     print(get_all(connection))
+    print(add_product(connection,{
+        'product_name':'spinach',
+        'um_id':'1',
+        'price_per_unit':'67'
+    }))
