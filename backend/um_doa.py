c86fa02415c85b71bc5b29236df91a30b44f6dfc
Affaan Jaweed <affaan190@gmail.com>
2024-11-29 17:44:36 +0530

    added um file

---
+++ backend/um_doa.py
@@ -0,0 +1,19 @@
+def get_um(connection):
+    cursor=connection.cursor()
+
+    query=('Select * from um')
+    cursor.execute(query)
+    response=[]
+    for(um_id,um_name) in cursor:
+        response.append({
+            'um_id':um_id,
+            'um_name':um_name
+        })
+    return response
+if __name__=='__main__':
+
+    from mysql_connector import sql_con_obj
+
+    connection=sql_con_obj()
+    x=get_um(connection)
+    print(list(x))
