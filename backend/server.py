061d42629011baeb5f8c36299778979cdb227b04
Affaan Jaweed <affaan190@gmail.com>
2024-11-29 17:43:44 +0530

    added few more routes

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,jsonify
 import python_doa
 import mysql_connector
-
+import um_doa
 app=Flask(__name__)
 
 con_obj=mysql_connector.sql_con_obj()
@@ -23,10 +23,35 @@ def get_product():
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
+@app.route('/getUom',methods=['GET'])
+def get_um():
+    um=um_doa.get_um(con_obj)
+    response=jsonify(um)
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
+@app.route('/deleteProduct', methods=['POST'])
 
+def delete_product():
+    return_id = python_doa.delete_product(con_obj, request.form['product_id'])
+    response=jsonify({
+        'product_id': return_id
+    })
 
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
+@app.route('/insertProduct',methods=["POST"])
+
+def insert_product():
+    insertion=python_doa.add_product(con_obj,request.form['data'])
+    response=jsonify({
+        'product_id': insertion
+    })
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
 
 
 if __name__=='__main__':
