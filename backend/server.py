c33e7186c05be794c5b9543cb50e144f804b916c
Affaan Jaweed <affaan190@gmail.com>
2024-11-30 07:24:27 +0530

    Added order function roots

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,jsonify
 import python_doa
 import mysql_connector
+import order_doa
 import um_doa
+import json
 app=Flask(__name__)
 
 con_obj=mysql_connector.sql_con_obj()
@@ -44,7 +46,7 @@ def delete_product():
 @app.route('/insertProduct',methods=["POST"])
 
 def insert_product():
-    insertion=python_doa.add_product(con_obj,request.form['data'])
+    insertion=python_doa.add_product(con_obj,json.loads(request.form['data']))
     response=jsonify({
         'product_id': insertion
     })
@@ -53,6 +55,28 @@ def insert_product():
 
     return response
 
+@app.route('/insertorder',methods=['POST'])
+def insert_order():
+    insertion=order_doa.insert_order(con_obj,json.loads(request.form['data']))
+    response=jsonify({
+        'order_id':insertion
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+
+@app.route('/getallorder',methods=['GET'])
+def get_all_orders():
+    orders=order_doa.get_all_orders(con_obj)
+    response=jsonify(orders)
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/getorderdetail/<int:order_id>',methods=['GET'])
+def get_order_details(order_id):
+    order_detail=order_doa.get_order_details(con_obj,order_id)
+    response=jsonify(order_detail)
+    return response
 
 if __name__=='__main__':
     print("Starting python flask server for store managment")
